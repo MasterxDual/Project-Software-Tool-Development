5549a3167d890ae4bfa90ed0db2492185eb4f5d6
Agustin Brambilla <agustinbram@gmail.com>
2025-01-20 13:13:27 -0300

    Adding inputs

--- input/test.c
+++ input/test.c
@@ -1,13 +1,237 @@
 #include <stdio.h>
 #include <math.h>
 
-double hola = 77777.77777;
-
 void main() {
-    int t = hola;
+    // int a = 0, b = 2;
 
-    if(t) 
-        t = t * 2;
+    // int t = a || b;   
+    // int y = a && b;
+
+    // int y = 0;
+    // int x = 1;
+    // int w = 2;
+
+    // while (y != 3 * 2 + (5 * 1) / 2 && x != 0 && w != 0) {
+    //     x = x + 1;
+    //     w = w + 1;
+
+	//     y = w * x;
+    // }
+
+    /* Esto se puede traducir como: */
+    // label l0
+    // t0 = y < 100
+    // ifnjmp t0, l1
+    // t1 = w * x
+    // y = t1
+    // jmp l0
+    // label l1
+
+    int w = 2;
+
+    while (1 + 2) {
+        w = w + 1;
+    }
+
+
+    /* 
+    a = 0
+
+    b = 2
+
+    t1 = a || b
+    t = t1
     
-    printf("%d", t);
+    t2 = a && b
+    y = t2
+
+    */
+
+    // printf("%d\n%d", t, y);
+
+    // int a = 5;
+    // int b = !a;
+
+    // /*
+
+    // a = 5 
+    
+    // t0 = !a 
+    // b = t0
+
+    //  */
+
+    // a = b + !10;
+
+    // /* 
+    
+    // t1 = !10
+    // t2 = b + t1
+    // a = t2
+
+    //  */
+
+    // a = !(10 - 11);
+
+    /* 
+    
+    t1 = 10 - 11
+    t2 = !t1
+    a = t2
+
+    */
+
+
+
+    // int d = 5 < (5 + 11) * 4 - (1 + (2 + 3)) * 2;
+
+    /* 
+    
+    t0 = 5 + 11
+    t1 = t0 * 4
+    t2 = 2 + 3
+    t3 = 1 + t2
+    t4 = t3 * 2
+    t5 = t1 - t4
+    t6 = 5 < t5
+    d = t6
+
+    
+    */
+    
+    // int a = (5 < 1) * 4 * 3 >= 10 / 2;
+
+    /* 
+    
+    t0 = 5 < 1
+    t1 = 4 * 3
+    t2 = t0 * t1
+    t3 = 10 / 2
+    t4 = t2 >= t3
+    a = t4
+    
+    */
+
+
+    /* 
+    
+    t0 = 4 * 3
+    t1 = 1 + t0
+    t2 = 5 < t1
+    t3 = 10 / 2
+    t4 = t2 >= t3
+    a = t4
+
+    int a = term1 >= term2
+    term1 = subterm1 < subterm2
+    subterm1 = 5
+    subterm2 = subterm3 + subterm4
+    subterm3 = 1
+    subterm4 = 4 * 3
+
+    term2 = subterm5
+    subterm5 = 10 / 2
+
+     */
+
+    /* 
+    
+    t0 = 
+    
+    */
+
+    // int c = 5 < 9 != 10 < 2 > 23 * 3;
+
+    /*
+
+    t0 = 5 < 9
+    t1 = 10 < 2
+    t2 = 23 * 3
+    t3 = t1 > t2
+    t4 = t0 != t3
+    c = t4
+
+    */
+
+
+
+    /* 
+
+    t0 = 5 >= 9
+    t1 = 10 < 2
+    t2 = t1 > 23
+    t3 = t0 == t2
+    c = t3
+
+    */
+
+    /* 
+    
+    t0 = 4 * 3
+    t1 = 1 + t0
+    t2 = 10 / 2
+    t3 = 5 < t1
+    t4 = t3 >= t2
+    a = t4
+
+    
+    */
+    
+    
+    // /* Declaraciones */
+    // int a = (5 + 3) * 2 - 4 + 1 / (1 / 2);
+
+    // int x = 3 + (1 + 2 / 2) * 3 + 3;
+    
+    // int y = 2 - 3 * (2 * 4 - 1) - 1;
+    
+    // int z = 2 - (2 * 4 - 1) - 1;
+
+    // /* Asignaciones */
+    // x = 3 * y + (5 * z) / 2;
+    // z = 0;
+    // y = 10 + 20;
+
+    /*
+    
+    t0 = 3 * y
+    t1 = 5 * z
+    t2 = t1 / 2
+    t3 = t0 + t2
+    x = t3
+
+    */
+
+    /*
+
+    t0 = 1 + 2
+    t1 = 3 * t0
+    t2 = t1 * 3
+    t3 = t2 + 3
+    x = t3
+
+     */
+
+    // int y = 1 + (2 + 3 * 5);
+
+
+
+
+
+    // int y = 1;
+    // int z = 0;
+    // int x = (3 * 5) / y + 5 * (z / (2 * 3)) - 1;
+
+
+    // int w = 3 * (2 * (4 * 8)) + ((1 * 2) * 6);
+
+
+
+
+
+/*  int a = 7 < 5 != 0 >= 1;
+    int c = !a;
+
+    printf("%d\n", a);
+    printf("%d\n", c); */
 }
